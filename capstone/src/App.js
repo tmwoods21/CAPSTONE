c7b4a5aa368a45acebb2470e7812c159d1b293f5
Taylor Woods <temwoods@gmail.com>
2022-03-10 17:03:06 -0500

    trial and error

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Route, Link, Routes } from 'react-router-dom';
+
 import './App.css';
+import './components/navbar'
+import Navbar from './components/navbar';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      <div className="App">
+        <Navbar />
+        <h2>Welcome to No Name!</h2>
+
+        <Routes>
+
+        </Routes>
+      </div>
+  
   );
 }
 
---
+++ capstone/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    return(
+        <nav>
+            <a href="/">Home</a>
+            <a href="">Resources</a>
+            <a href="/informationpage">Information</a>
+            <a href="testimonies">Testimonies</a>
+        </nav>
+    )
+}
+
+export default Navbar;
