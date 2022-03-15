e8998d85d8de11a7251bd6cff146bf24aac87ed9
Taylor Woods <temwoods@gmail.com>
2022-03-15 16:34:59 -0400

    errors f

--- src/views/testimonies.jsx
+++ src/views/testimonies.jsx
@@ -18,7 +18,7 @@ function Testimonies(){
                 <p>
                 Summary: Tanya J. Peterson, MS, NCC, a counselor living with anxiety, writes this blog geared toward others going through similar experiences. Her writing expertly mixes the personal and professional, with topics ranging from causes and stigmas to checklists for controlling anxiety and panic attacks. The guest posts offer another perspective on anxiety disorders, making this a blog with a lot of insight. Tanya has covered a variety of topics, including insomnia and anxiety, stress eating, and anxious memories.
                 <br/>
-                <a href="https://www.healthyplace.com/blogs/anxiety-schmanxiety" target="_blank">Tanya's Blog</a>
+                <a href="https://www.healthyplace.com/blogs/anxiety-schmanxiety" rel="noreferrer" target="_blank">Tanya's Blog</a>
                 </p>
                 
                 <h3>&#127807;Name: Meredith</h3>
@@ -26,7 +26,7 @@ function Testimonies(){
                 <p>
                 Summary: Meredith Arthur started this blog after being diagnosed with Generalized Anxiety Disorder and wanting to find more treatment options for her chronic migraines. “Diagnosis in hand, I searched and searched but found nothing online that spoke to my physical experience of stress, overthinking, and anxiety,” she writes. She also has a podcast on the topic.
                 <br/>
-                <a href="https://bevoya.com/blog" target="_blank">Meredith's Blog</a>
+                <a href="https://bevoya.com/blog" target="_blank" rel="noreferrer">Meredith's Blog</a>
                 </p>
             </div>
 
